869c3679cca9aed84aa39ecbc0ec12f9fdeb0d68
Rodrigo Volpe <kild_ro@hotmail.com>
2021-04-16 10:57:01 -0300

    Commit do Layoult inicial

--- app/src/main/java/com/mineiro/lista_cardview/ContactAdapter.kt
+++ app/src/main/java/com/mineiro/lista_cardview/ContactAdapter.kt
@@ -1,26 +1,45 @@
 package com.mineiro.lista_cardview
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHolder>() {
-
     private val list: MutableList<Contact> = mutableListOf()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactAdapterViewHolder {
-        TODO("Not yet implemented")
+        //acontece 1 passo antes do onBindViewHolder()
+        //Onde vou criar a viewHolder (criar o layoult e inflá-lo)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.contact_item, parent, false)
+        return ContactAdapterViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ContactAdapterViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        //onde vou popular cada card da ViewHoder
+        holder.bind((list[position]))
     }
 
     override fun getItemCount(): Int {
         return list.size
     }
 
-    class ContactAdapterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    fun updateList(listUp: List<Contact>) {
+        this.list.clear()
+        this.list.addAll(listUp)
+        notifyDataSetChanged()   //indicando que a lista mudou, e terá que passar em todos os metodos acima novamente.
+    }
 
+    class ContactAdapterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val tvNome: TextView = itemView.findViewById(R.id.tv_nome)
+        private val tvTelefone: TextView = itemView.findViewById(R.id.tv_telefone)
+        private val ivFoto: TextView = itemView.findViewById(R.id.iv_foto)
+
+        fun bind(contact: Contact) {
+            tvNome.text = contact.nome
+            tvTelefone.text = contact.telefone
+            ivFoto.text = contact.foto
+        }
     }
 }
--- app/src/main/java/com/mineiro/lista_cardview/MainActivity.kt
+++ app/src/main/java/com/mineiro/lista_cardview/MainActivity.kt
@@ -2,10 +2,41 @@ package com.mineiro.lista_cardview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private val rvList: RecyclerView by lazy {
+        findViewById<RecyclerView>(R.id.rv_List)
+    }
+    private val adapterMain = ContactAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bindViews()
+        updateList()
+    }
+
+    private fun bindViews() {
+        rvList.adapter = adapterMain    //fiz a união dos 2 adapters
+        rvList.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun updateList() {
+        adapterMain.updateList(
+            arrayListOf(
+                Contact(
+                    "Rodrigo Volpe",
+                    "(21) 99222-3456",
+                    "teste.png"
+                ),
+                 Contact(
+                    "Rodrigo Volpe",
+                    "(21) 99222-3456",
+                    "teste.png"
+                )
+            )
+        )
     }
 }
